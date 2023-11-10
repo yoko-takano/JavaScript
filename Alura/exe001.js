b0c1bca0f47882718316bb55775b9c583e69c080
Yoko Lucila Takano <148287663+yoko-takano@users.noreply.github.com>
2023-11-10 10:10:44 -0400

    organização e acréscimo javascript alura

---
+++ Alura/exe001.js
@@ -0,0 +1,12 @@
+const idade = 2;
+const nome = "Lara";
+const sobrenome = "Ayumi";
+bacharel = false;
+dezAnosDepois = "12";
+
+const nomeCompleto = nome + sobrenome;
+const resultado = idade + dezAnosDepois;
+console.log(nomeCompleto);
+console.log(resultado);
+console.log(bacharel == 1);
+
